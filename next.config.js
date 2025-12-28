481405468fb4c3ffb9d3dd5c8f27bc6ccc28b4b5
kkkei257 <52337039+kkkei257@users.noreply.github.com>
2025-12-29 00:11:22 +0900

    WORKへのコンテンツ追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn-ak.f.st-hatena.com',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'qiita-user-contents.imgix.net',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
